ef4ea856db4a64d9901ab5c2a89360dd48b2748c
richhh7g <richhh7g@protonmail.com>
2024-02-28 14:52:53 -0300

    feat: common paths util

--- src/core/util/constants.ts
+++ src/core/util/constants.ts
@@ -1,4 +1,12 @@
+import path from "path";
+
 export const IS_TEST = process.env.NODE_ENV === "test";
 export const IS_SERVER = ["development", "production"].includes(
   process.env.NODE_ENV as string
 );
+
+export const ROOT_PATH = path.resolve(__dirname, "..", "..", "..");
+export const BASE_PATH = path.resolve(__dirname, "..", "..");
+export const CORE_PATH = path.resolve(BASE_PATH, "core");
+export const API_PATH = path.resolve(BASE_PATH, "api");
+export const TEST_PATH = IS_TEST && path.resolve(BASE_PATH, "test");
